f206a76c35ca73e5c08646426f8050c8f3a316fa
yogita109 <ush109rock@gmail.com>
2020-08-03 12:23:25 +0900

    スキルシート更新

--- src/repository/skill.repository.js
+++ src/repository/skill.repository.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default function () {
-  return axios.get('https://gist.githubusercontent.com/yogita109/d9650b5e19f0174c8a744a21a5232db4/raw/daf4e5702ad50f83d600194805f7dc2dac5fcd71/skill-sheet.md')
+  return axios.get('https://gist.githubusercontent.com/yogita109/d9650b5e19f0174c8a744a21a5232db4/raw/a37536251f17951a7784b3c539e517edee9aea33/skill-sheet.md')
 }
